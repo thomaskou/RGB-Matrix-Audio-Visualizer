5b1e66334f39333530abe1336657c7d1cb7918df
t4kou <thomaskou00@gmail.com>
2018-10-24 20:56:08 -0400

    Small push.

--- src/Main.py
+++ src/Main.py
@@ -12,6 +12,7 @@ def get_filepath(filename):
 stream = aus.AudioStream()
 
 # stream.set_wave_path(get_filepath('test_triangle_arpeggio.wav'))
-stream.set_wave_path('./audio/test_triangle_arpeggio.wav')
+# stream.set_wave_path('./audio/test_triangle_arpeggio.wav')
+stream.set_wave_path('./audio/downloaded/IKSON_PARADISE.wav')
 stream.init_audio_stream_wav(1024)
 stream.play_full_stream_wav()
