96431c80dd5a46e15c29dec56d2df6f705b31384
t4kou <thomaskou00@gmail.com>
2018-10-24 21:02:23 -0400

    Small push 2.

--- src/Main.py
+++ src/Main.py
@@ -2,17 +2,13 @@ import os
 import audio.AudioStream as aus
 
 
-"""
 def get_filepath(filename):
-    dir = os.path.dirname(__file__)
-    return os.path.join(dir, 'audio', filename)
-"""
+    return os.path.join('./audio/' + filename)
 
 
 stream = aus.AudioStream()
 
 # stream.set_wave_path(get_filepath('test_triangle_arpeggio.wav'))
-# stream.set_wave_path('./audio/test_triangle_arpeggio.wav')
-stream.set_wave_path('./audio/downloaded/IKSON_PARADISE.wav')
+stream.set_wave_path(get_filepath('downloaded/IKSON_PARADISE.wav'))
 stream.init_audio_stream_wav(1024)
 stream.play_full_stream_wav()
