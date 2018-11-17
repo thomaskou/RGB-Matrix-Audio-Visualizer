3b2af00fd0da05bb12c3740dd517fc48c653cd7d
t4kou <thomaskou00@gmail.com>
2018-11-16 21:29:57 -0500

    Added an indefinite microphone input feature (program only terminates when the process is stopped).

--- src/Main.py
+++ src/Main.py
@@ -1,17 +1,19 @@
-# import os
-# import numpy as np
+import keyboard
 import audio.AudioStream as aStr
 import audio.InputStream as aInp
 import audio.AudioSpectrum as aSpec
 import audio.SpectrumAnalysis as aSpa
 
 
-INPUT = False
-CHUNK = 2048
+INPUT = True
+TIMED = False
+SECONDS = 10
+
 FILE = 'test_triangle_arpeggio.wav'
 # FILE = 'downloaded/IKSON_PARADISE.wav'
 # FILE = 'downloaded/you_already_know_what_it_is.wav'
-SECONDS = 10
+
+CHUNK = 2048
 
 
 # Local functions.
@@ -85,7 +87,42 @@ def input_seconds():
     inp.stop_input_stream()
 
 
+def input_indefinite():
+
+    # Instantiate relevant classes.
+    inp = aInp.InputStream()
+    spec = aSpec.AudioSpectrum()
+    span = aSpa.SpectrumAnalysis()
+
+    # Initialize class stuff.
+    inp.init_input_stream(CHUNK)
+    span.set_frequencies(0, inp.get_framerate())
+
+    # Initialize drawing (for testing only).
+    # span.plot_init()
+    # span.draw_init()
+
+    # Work through entire audio stream.
+    while True:
+        inp.tick_input_stream()
+        try:
+            spec.set_audio_data(inp.get_input_data())
+            spec.data_to_spectrum()
+            # print(abs(spec.get_spectrum()[0]))
+            # print(spec.get_spectrum()[1])
+            span.set_spectrum(spec.get_spectrum())
+            # print(span.get_amplitude_array(16))
+            span.plot_update()
+            span.plot_pause(0.000000001)
+        except ValueError:
+            break
+    inp.stop_input_stream()
+
+
 if INPUT:
-    input_seconds()
+    if TIMED:
+        input_seconds()
+    else:
+        input_indefinite()
 else:
     play_file()
